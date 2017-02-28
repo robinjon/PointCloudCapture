7f47dd16b1396df5a0fad5a84ec1db1f84f64f86
ROBINS-LAPTOP\Robin <Robinjon89@gmail.com>
2017-02-28 15:03:43 +0100

    Issue #1: Added kinect connection and openGL window
    Issue #2: Refactored code and added comments

---
+++ PointCloudCapture/PointCloudSources/Kinect/Parameters/CameraParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointCloudCapture.PointCloudSources.Kinect.Parameters
+{
+    /// <summary>
+    /// Camera parameter unique for the Kinect V1
+    /// </summary>
+    struct CameraParameters
+    {
+        public const int WIDTH = 640;
+        public const int HEIGHT = 480;
+
+        public const float CX = WIDTH / 2;
+        public const float CY = HEIGHT / 2;
+        public const float A = 0.00173667f;
+
+        public const int MIN_KINECT_VALUE = 0;
+        public const int MAX_KINECT_VALUE = 4095;
+    }
+}
